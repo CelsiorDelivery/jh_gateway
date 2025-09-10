103294043f9b6bf33fcfc6826ffb44bb113fe6c4
Istiyak-Celsior <md.istayaque@celsiortech.com>
2025-09-10 17:02:19 +0530

    added token check

--- jh_gateway/Middlewares/JwtMiddleware.cs
+++ jh_gateway/Middlewares/JwtMiddleware.cs
@@ -4,22 +4,44 @@ using System.Text;
 
 namespace jh_gateway.Middlewares
 {
+    /// <summary>
+    /// 
+    /// </summary>
     public class JwtMiddleware
     {
         private readonly RequestDelegate _next;
         private readonly IConfiguration _configuration; // Inject IConfiguration to access settings
 
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="next"></param>
+        /// <param name="configuration"></param>
         public JwtMiddleware(RequestDelegate next, IConfiguration configuration)
         {
             _next = next;
             _configuration = configuration;
         }
 
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="context"></param>
+        /// <returns></returns>
         public async Task Invoke(HttpContext context)
         {
             var token = context.Request.Headers["Authorization"].FirstOrDefault()?.Split(" ").Last();
 
-            if (token != null)
+            if (token == null)
+            {
+                if (!ValidateRoute(context))
+                {
+                    throw new UnauthorizedAccessException("You are not authorize to access this url.");
+                }
+
+                await _next(context);
+            }
+            else
             {
                 await AttachUserToContext(context, token);
             }
@@ -27,12 +49,29 @@ namespace jh_gateway.Middlewares
             await _next(context);
         }
 
+        private bool ValidateRoute(HttpContext context)
+        {
+            if (context.Request.Path.ToString().ToLower().Contains("/login") ||
+                context.Request.Path.ToString().ToLower().Contains("/registration"))
+            {
+                return true;
+            }
+
+            return false;
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="context"></param>
+        /// <param name="token"></param>
+        /// <returns></returns>
         private async Task AttachUserToContext(HttpContext context, string token)
         {
             try
             {
                 var tokenHandler = new JwtSecurityTokenHandler();
-                var key = Encoding.ASCII.GetBytes(_configuration["Jwt:Secret"]); // Get secret key from configuration
+                var key = Encoding.ASCII.GetBytes(_configuration["Jwt:Key"]!); // Get secret key from configuration
 
                 tokenHandler.ValidateToken(token, new TokenValidationParameters
                 {
@@ -46,6 +85,7 @@ namespace jh_gateway.Middlewares
                 var jwtToken = (JwtSecurityToken)validatedToken;
                 // You can extract claims from jwtToken.Claims here and populate user context
                 // For example, context.Items["User"] = new User { Id = int.Parse(jwtToken.Claims.First(x => x.Type == "id").Value) };
+                await Task.CompletedTask;
             }
             catch
             {
